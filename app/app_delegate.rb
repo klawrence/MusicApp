bcd19a1c19bafa9e29482cc806dafd09db81a761
kevin <kevin@diamond-sky.com>
2018-05-19 19:41:49 -0700

    Add a play button

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,10 +1,8 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    rootViewController = UIViewController.alloc.init
-    rootViewController.title = 'MusicApp'
-    rootViewController.view.backgroundColor = UIColor.whiteColor
-
-    navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
+    playerController = PlayerController.new
+    navigationController = UINavigationController.alloc.
+        initWithRootViewController(playerController)
 
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = navigationController
---
+++ app/controllers/player_controller.rb
@@ -0,0 +1,18 @@
+class PlayerController < UIViewController
+  def viewDidLoad
+    super
+
+    self.title = 'MusicApp'
+    self.view.backgroundColor = UIColor.whiteColor
+    self.view.addSubview create_play_button
+  end
+
+  def create_play_button
+    @play_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @play_button.setTitle('▶', forState: UIControlStateNormal)
+    @play_button.font = UIFont.boldSystemFontOfSize(80.0)
+    @play_button.sizeToFit
+    @play_button.center = CGPointMake(self.view.frame.size.width / 2, 200)
+    @play_button
+    end
+end
