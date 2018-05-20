89221c7eb8ddc23622c4843cc8411c3b4a7d1728
kevin <kevin@diamond-sky.com>
2018-05-20 09:42:19 -0700

    Add a search controller and use a tab bar controller

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -5,7 +5,7 @@ class AppDelegate
     player_controller = PlayerController.new
     search_controller = SearchController.new
 
-    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    tab_controller = UITabBarController.new
     tab_controller.viewControllers = [player_controller, search_controller]
 
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
